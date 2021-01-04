950484bf8fbd08b0dcf1112c38f59e3ca8ddcdf9
themonty <themonty@nate.com>
2021-01-04 22:55:18 +0900

    first commit

---
+++ fc_community/fcuser/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from .models import Fcuser
+
+# Create your views here.
+
+def register(request):
+    
+    if request.method == "GET":
+        return render(request, 'register.html')
+    elif request.method == "POST":
+        username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password')
+
+        res_data = {}
+
+        if not (username and password and re_password, useremail):
+            res_data['error'] = "모든 값을 입력해야 합니다."
+        elif password != re_password:
+            res_data['error'] = '비밀번호가 다릅니다.'
+        else:
+            fcuser = Fcuser(
+                username = username,
+                useremail = useremail,
+                password = make_password(password)
+            )
+
+            fcuser.save()
+
+        return render(request, 'register.html', res_data)
