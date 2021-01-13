9dfe862e73b8853c6fed1db3ef0dd1e71888b31a
themonty_회사 <ljs@hogun.co.kr>
2021-01-13 17:52:44 +0900

    회사 처리

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -1,9 +1,48 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth.hashers import make_password
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password, check_password
 from .models import Fcuser
 
 # Create your views here.
+
+def home(request):
+    user_id = request.session.get('user')
+
+    if user_id:
+        fcuser = Fcuser.objects.get(pk=user_id)
+        return HttpResponse(fcuser.username)
+        
+    return HttpResponse('Home!')
+
+def login(request):
+
+    if request.method == 'GET':        
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        res_data = {}
+
+        if not (username and password):
+            res_data['error'] = '모든 값을 입력해야 합니다.'
+        else:
+            fcuser = Fcuser.objects.get(username=username)
+            if check_password(password, fcuser.password):
+                # 비밀번호 일치, 로그인 처리
+                # 세션
+                request.session['user'] = fcuser.id
+                return redirect('/')
+
+                pass
+            else:
+                res_data['error'] = '비밀번호를 틀렸습니다.'
+        
+        return render(request, 'login.html', res_data)
+
+    
+
+
 
 def register(request):
     
